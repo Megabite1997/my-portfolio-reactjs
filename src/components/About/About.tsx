8c5ea1e3e389af8491ade98719b321121f492c48
Tada Chaipanya <fendersdtua@hotmail.com>
2024-10-13 15:12:42 +0700

    Update responsive

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from "react";
+import { FC } from "react";
 import styles from "./About.module.css";
 import { getImageURL } from "../../utils";
 
--- src/components/Projects/ProjectCard.tsx
+++ src/components/Projects/ProjectCard.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from "react";
+import { FC } from "react";
 import styles from "./ProjectCard.module.css";
 import { getImageURL } from "../../utils";
 
